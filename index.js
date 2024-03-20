118a21fcba622e6b8426eedab41b0a140150c03f
joel langat <langatjoel42@gmail.com>
2024-03-20 14:28:55 +0300

    made some functions in index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction (callback){
+    callback();
+
+}
+
+function returnsANamedFunction(){
+    return function joel(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
